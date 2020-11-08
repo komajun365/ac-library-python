fb049b398847e6406dd64196e712524ed84f8a10
komajun365 <komajun365@gmail.com>
2020-11-08 19:58:12 +0900

    docstringを記述した

--- atcoder/internal_bit.py
+++ atcoder/internal_bit.py
@@ -1,12 +1,37 @@
-#  @param n `0 <= n`
-#  @return minimum non-negative `x` s.t. `n <= 2**x`
-def ceil_pow2(n: int):
+def ceil_pow2(n):
+    """
+    n <= 2**x を満たす最小のxを返却する。
+
+    Parameters
+    ----------
+    n : int
+        0以上の整数
+
+    Returns
+    -------
+    int
+        n <= 2**x を満たす最小のx
+
+    """
     if n < 1:
         return 0
     return (n-1).bit_length()
 
 
-# @param n `1 <= n`
-# @return minimum non-negative `x` s.t. `(n & (1 << x)) != 0`
-def bsf(n: int):
+def bsf(n):
+    """
+    自然数を2bitで表現したときに、右から見て最初に1が立つ桁が何桁目かを返却する。
+    （0-indexed）
+
+    Parameters
+    ----------
+    n : int
+        1以上の整数
+
+    Returns
+    -------
+    int
+        2bitで表現したときに、右から見て最初に1が立つ桁（0-indexed）
+
+    """
     return (n & -n).bit_length()-1
