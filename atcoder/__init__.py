26513508a43937373674770da2e497973c88a697
komajun365 <komajun365@gmail.com>
2021-01-04 19:12:45 +0900

    mathパッケージとの競合を避けるため、モジュール名を「math_acl」に変更

--- atcoder/__init__.py
+++ atcoder/__init__.py
@@ -4,7 +4,7 @@ from atcoder.lazysegtree import LazySegTree
 from atcoder.internal_math import is_prime, inv_gcd, primitive_root
 from atcoder.convolution import Convolution
 from atcoder.maxflow import MaxFlow
-from atcoder.math import inv_mod, crt, floor_sum
+from atcoder.math_acl import inv_mod, crt, floor_sum
 
 __all__ = [
     'Sample', 'ceil_pow2', 'bsf', 'LazySegTree', 'is_prime', 'inv_gcd',
